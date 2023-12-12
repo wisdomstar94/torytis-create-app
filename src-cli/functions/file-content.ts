9a2884635379f50951b739cb7083e648ba4d47e2
wisdomstar94 <wisdomstar94@gmail.com>
2023-12-12 13:34:07 +0900

    v0.0.37 : 패키지 버전 최신화 및 프로젝트 생성시 생성되는 package.json 의 패키지 버전 최신화

--- src-cli/functions/file-content.ts
+++ src-cli/functions/file-content.ts
@@ -13,16 +13,16 @@ export const packageJsonContent = (params: PackageJsonContentParams) => `
     "build:variable": "torytis build:variable"
   },
   "devDependencies": {
-    "@types/node": "^20.9.2",
-    "@types/react": "^18.2.37",
-    "@types/react-dom": "^18.2.15",
+    "@types/node": "^20.10.4",
+    "@types/react": "^18.2.43",
+    "@types/react-dom": "^18.2.17",
     "react": "^18.2.0",
     "react-dom": "^18.2.0",
-    "tailwindcss": "^3.3.5",
+    "tailwindcss": "^3.3.6",
     "postcss-modules": "^6.0.0",
-    "typescript": "^5.2.2",
-    "esbuild": "^0.19.6",
-    "@wisdomstar94/torytis": "^0.0.36"
+    "typescript": "^5.3.3",
+    "esbuild": "^0.19.9",
+    "@wisdomstar94/torytis": "^0.0.37"
   }
 }
 `.trim();
